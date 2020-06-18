e95671d059d82120e472c2254ca1ea12b680c286
fabio.mota <fabio.mota@hostelworld.com>
2020-06-18 13:37:51 +0100

    adding cors for netlify

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: ["http://localhost:8081", "http://localhost:3000"]
+  origin: ["http://localhost:8081", "http://localhost:3000", "https://nuxt-soccer-players.netlify.app"]
 };
 
 app.use(cors(corsOptions));
